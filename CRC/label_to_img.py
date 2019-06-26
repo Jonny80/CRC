bd84419b9b7c58590af70e33ef0db0643f7e17ef
Jonny80 <jan@mhaendl.de>
2019-06-26 17:25:44 +0200

    tensor to image

--- CRC/label_to_img.py
+++ CRC/label_to_img.py
@@ -11,6 +11,20 @@ import random
 import time
 import numpy
 
+mapping = {
+            0:(0, 0, 0, 255),  # background,
+            1:(0, 255, 0, 255),  # instrument-shaft
+            2:(0, 255, 255, 255),  # instrument-clasper
+            3:(125, 255, 12, 255),  # instrument-wrist
+            4:(255, 55, 0, 255),  # kidney-parenchyma
+            5:(24, 55, 125, 255),  # covered-kidney
+            6:(187, 155, 25, 255),  # thread
+            7:(0, 255, 125, 255),  # clamps
+            8:(255, 255, 125, 255),  # suturing-needle
+            9:(123, 15, 175, 255),  # suction-instrument
+            10:(124, 155, 5, 255)  # small-intestine
+        }
+
 def label_to_img(batch, img_size): #[4, 11, 128, 128]
     height = img_size
     width = img_size
@@ -55,3 +69,17 @@ def label_to_img(batch, img_size): #[4, 11, 128, 128]
             bottom = (height + new_height) / 2
             label_as_img = label_as_img.crop((left, top, right, bottom))'''
     return out_img
+
+
+
+def tensor_to_img(batch,img_size):
+    element = batch[0].numpy()
+    out_img = Image.new("RGBA",(img_size,img_size),(0, 0, 0, 255))
+
+    for i in range(img_size):
+        for j in range(img_size):
+            value = mapping[element[i][j]]
+            out_img.putpixel((i,j),value)
+
+
+    
