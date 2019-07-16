48bd8dd1be1a7711b0bfa904b4e92d92f214f06b
Jonny80 <jan@mhaendl.de>
2019-07-16 13:46:57 +0200

    dice Multiclasses

--- CRC/dice.py
+++ CRC/dice.py
@@ -1,10 +1,47 @@
 import torch
+from torch.nn import functional as F
 
-def dice_loss(pred, target):
-    smooth = 1.
-    num = pred.size(0)
-    m1 = pred.view(num, -1)  # Flatten
-    m2 = target.view(num, -1)  # Flatten
-    intersection = (m1 * m2).sum()
+def dice_loss(input,target):
 
-    return (2. * intersection + smooth) / (m1.sum() + m2.sum() + smooth)
+    smooth = 1
+
+    iflat = input.view(-1)
+    tflat = target.view(-1)
+    intersection = (iflat * tflat).sum()
+    return 1 - ((2. * intersection + smooth) / (iflat.sum() + tflat.sum() + smooth))
+
+
+
+def dice_loss2(true, logits, eps=1e-7):
+    """Computes the Sørensen–Dice loss.
+    Note that PyTorch optimizers minimize a loss. In this
+    case, we would like to maximize the dice loss so we
+    return the negated dice loss.
+    Args:
+        true: a tensor of shape [B, 1, H, W].
+        logits: a tensor of shape [B, C, H, W]. Corresponds to
+            the raw output or logits of the model.
+        eps: added to the denominator for numerical stability.
+    Returns:
+        dice_loss: the Sørensen–Dice loss.
+    """
+    num_classes = logits.shape[1]
+    if num_classes == 1:
+        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        true_1_hot_f = true_1_hot[:, 0:1, :, :]
+        true_1_hot_s = true_1_hot[:, 1:2, :, :]
+        true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
+        pos_prob = torch.sigmoid(logits)
+        neg_prob = 1 - pos_prob
+        probas = torch.cat([pos_prob, neg_prob], dim=1)
+    else:
+        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        probas = F.softmax(logits, dim=1)
+    true_1_hot = true_1_hot.type(logits.type())
+    dims = (0,) + tuple(range(2, true.ndimension()))
+    intersection = torch.sum(probas * true_1_hot, dims)
+    cardinality = torch.sum(probas + true_1_hot, dims)
+    dice_loss = (2. * intersection / (cardinality + eps)).mean()
+    return (1 - dice_loss)
--- CRC/run_unet.py
+++ CRC/run_unet.py
@@ -4,7 +4,8 @@ from label_to_img import *
 from dice import *
 import sys
 import torch
-import loss
+from torch import nn
+import losses
 
 use_gpu = torch.cuda.is_available()
 
@@ -31,7 +32,7 @@ def main():
         except:
             generator = UnetGenerator(3, 11, 64).cuda()# (3,3,64)#in_dim,out_dim,num_filter out dim = 4 oder 11
             print("new model generated")
-        loss_function = LossMulti
+        loss_function = losses.LossMulti()
         optimizer = torch.optim.SGD(generator.parameters(), lr=learning_rate, momentum=momentum)
         #optimizer = torch.optim.Adam(generator.parameters(), lr=learning_rate)
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, mode='max', verbose=True)
@@ -44,10 +45,10 @@ def main():
                 label_batch = Variable(label_batch).cuda(0)
                 generated_batch = generator.forward(input_batch)
                 loss = loss_function(generated_batch,label_batch)
-                #dice = dice_loss(generated_batch, label_batch.cuda()).item()
-                #dice_sum += dice
+                dice = dice_loss2(label_batch, generated_batch.cuda()).item()
                 loss.backward()
                 optimizer.step()
+                dice_sum += dice
             avg_dice = dice_sum/train_loader.__len__()
             print("epoche:{}/{} avg dice:{}".format(ep, epoch - 1, avg_dice))
             scheduler.step(avg_dice)
@@ -69,11 +70,11 @@ def main():
         for batch_number, (input_batch, label_batch) in enumerate(validate_loader):
             input_batch = Variable(input_batch).cuda(0)
             generated_batch= generator.forward(input_batch)
-            #dice = dice_loss(generated_batch, label_batch.cuda()).item()
-            #dice_sum += dice
-            #print("batch:{}/{} dice: {}".format(batch_number, validate_loader.__len__()-1, dice))
+            dice = dice_loss2(generated_batch, label_batch.cuda()).item()
+            dice_sum += dice
+            print("batch:{}/{} dice: {}".format(batch_number, validate_loader.__len__()-1, dice))
             generated_out_img = label_to_img(generated_batch.cpu().data, img_size)
-            label_out_img = label_to_img(label_batch.cpu().data, img_size)
+            label_out_img = tensor_to_img(label_batch.cpu().data, img_size)
             generated_out_img.save("data/validate-result/img_{}_generated.png".format(batch_number))
             label_out_img.save("data/validate-result/img_{}_truth.png".format(batch_number))
         avg_dice = dice_sum / validate_loader.__len__()
